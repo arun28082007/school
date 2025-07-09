8ca678b311ee529971f1b7acb40bb6f7cd050897
unknown <arun28082007>
2025-07-09 10:55:53 +0530

    first

---
+++ ar.py
@@ -0,0 +1,23 @@
+student = {
+    "name": "Ali",
+    "age": 20,
+    "courses": ["Math", "Physics", "Chemistry"],
+    "grades": {
+        "Math": 85,
+        "Physics": 90,
+        "Chemistry": 88
+    },
+    "is_enrolled": True,
+    "address": {
+        "street": "123 Main St",
+        "city": "Cairo",
+        "country": "Egypt"
+    }
+}
+
+print("name:", student["name"])
+print("age:", student["age"])
+print("courses:", student["courses"])
+print("grades:", student["grades"])
+print("is_enrolled:", student["is_enrolled"])
+print("address:", student["address"])
