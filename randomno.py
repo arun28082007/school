dead275528673faadcb531c2f92abc97566093cf
arun28082007 <arunanand0807@gmail.com>
2025-07-12 10:18:46 +0530

    new codes

--- randomno.py
+++ randomno.py
@@ -1,8 +1,19 @@
 import random
+
+'''This script generates 100 random integers between 1 and 10,
+counts the frequency of each number, and prints the results.
+It uses the `Counter` class from the `collections` module to tally the occurrences.
+
 from collections import Counter
 
-results = [random.randint(1, 100) for _ in range(1000)]
+results = [random.randint(1, 10) for _ in range(100)]
 frequency = Counter(results)
 
-for number in range(1, 101):
+for number in range(1, 11):
     print(f"{number}: {frequency[number]}")
+'''
+
+for i in range(4):
+   #print(int(random.randint(1, 4  )))  # Generates 4 random floats between 1 and 4
+    print(int(100 + random.randint(1, 4)))  # Generates 4 random integers between 100 and 104
+
