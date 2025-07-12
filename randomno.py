e75442dbecd6ecea274028038db47c2420bb7e12
arun28082007 <arunanand0807@gmail.com>
2025-07-12 10:35:11 +0530

    city

--- randomno.py
+++ randomno.py
@@ -13,9 +13,24 @@ for number in range(1, 11):
     print(f"{number}: {frequency[number]}")
 '''
 
+'''
 for i in range(4):
     #print(int(random.randint(1, 4  )))  # Generates 4 random floats between 1 and 4
     #print(int(100 + random.randint(1, 4)))  # Generates 4 random integers between 100 and 104
-    print(int(random.random() * 5 + 5))  # Generates 4 random integers between 5 and 9
+    #print(int(random.random() * 5 + 5))  # Generates 4 random integers between 5 and 9
+'''
+
+'''
+for i in range(1000):
+    print(int((random.random())*5)+100,end=' ')
+
+# generate 1000 random integers between 100 and 104
+'''
 
 
+
+city = ['delhi', 'mumbai', 'chennai', 'kolkata',]
+pick = random.randint(0,3)
+for i in city:
+    for j in range(1, pick):
+        print(i, end=' ')
